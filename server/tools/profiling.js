0840466b9628262a335af803fdf8c6697487eba7
Jay Liang <jay.liang@ugeez.cn>
2017-01-30 23:50:14 +0800

    add profiling tool

---
+++ server/tools/profiling.js
@@ -0,0 +1,30 @@
+/**
+ * Created by jay on 30/01/2017.
+ */
+var Utils = require("weroll/utils/Utils");
+var Profiler = require("weroll/utils/Profiler");
+
+var proc;
+
+exports.init = function(invoker) {
+    proc = invoker;
+}
+
+exports.do = function(name, cmd) {
+    var profiler = new Profiler({ name:name });
+    if (cmd == "clean") {
+        return profiler.clean(function(err) {
+            proc.done(err);
+        });
+    }
+    profiler.view(function(err, result) {
+        if (result) {
+            console.log(`total requests: ${result.total}`);
+            console.log(`avg request time: ${result.avg}ms`);
+            console.log(`max request time: ${result.max}ms`);
+        }
+        proc.done(err);
+    }, function(obj) {
+        console.log(`${obj.target} ---> count: ${obj.count}     avg: ${obj.avg}ms     min: ${obj.min}ms     max: ${obj.max}ms`);
+    });
+}
